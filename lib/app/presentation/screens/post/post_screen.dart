2a4a6d261e8a220221c86a10a7a2e53e42959db4
drunisa007 <newarunkatwal@gmail.com>
2022-07-07 22:09:35 +0630

    post bg designing

--- lib/app/presentation/screens/post/post_screen.dart
+++ lib/app/presentation/screens/post/post_screen.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:session_http/app/presentation/screens/widgets/post_single_bg.dart';
 
 class PostScreen extends StatelessWidget {
   const PostScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Post Screen"),
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return PostSingleBg(
+          customChild: Container(),
+        );
+      },
+      itemCount: 10,
     );
   }
 }
