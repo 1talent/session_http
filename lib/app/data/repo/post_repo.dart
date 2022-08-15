86e2a8bdd7d6e35b4426b0fa0a8330a64920d976
drunisa007 <newarunkatwal@gmail.com>
2022-08-15 20:41:43 +0630

    Some changes

--- lib/app/data/repo/post_repo.dart
+++ lib/app/data/repo/post_repo.dart
@@ -4,13 +4,9 @@ import 'package:session_http/app/data/vos/post_vo.dart';
 import 'package:session_http/app/utils/api_helper.dart';
 
 class PostRepo {
-  late ApiHelper apiHelper;
-  PostRepo() {
-    apiHelper = ApiHelper();
-  }
 
   getPostList() async {
-    var result = await apiHelper.getData("post");
+    var result = await ApiHelper().getData("post");
     Map mResultMap = jsonDecode(result.body);
     List<PostVo> mPostList = postVoFromJson(jsonEncode(mResultMap['data']));
     return mPostList;
--- lib/app/utils/api_helper.dart
+++ lib/app/utils/api_helper.dart
@@ -1,9 +1,15 @@
 import 'package:http/http.dart' as http;
 
 class ApiHelper {
-  late String baseUrl;
-  ApiHelper() {
-    baseUrl = "https://dummyapi.io/data/v1/";
+   String baseUrl = "https://dummyapi.io/data/v1/";
+
+
+   static final ApiHelper _singleHelper = ApiHelper._internal();
+
+  ApiHelper._internal();
+
+  factory ApiHelper(){
+    return _singleHelper;
   }
 
   Future<dynamic> getData(String url) async {
